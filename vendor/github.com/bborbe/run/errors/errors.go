01dd4b6dc648edbba168f7177e23f54806c7713a
Benjamin Borbe <bborbe@rocketnews.de>
2018-02-09 18:44:59 +0100

    update run

--- vendor/github.com/bborbe/run/errors/errors.go
+++ vendor/github.com/bborbe/run/errors/errors.go
@@ -8,6 +8,14 @@ func New(errors ...error) errorList {
 	return errorList(errors)
 }
 
+func NewByChan(errors <-chan error) errorList {
+	var list []error
+	for err := range errors {
+		list = append(list, err)
+	}
+	return New(list...)
+}
+
 func (e errorList) Error() string {
 	buf := bytes.NewBufferString("errors: ")
 	first := true
